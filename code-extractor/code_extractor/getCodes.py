42a357d89e80c8be6cd6b5de40259d85fa13f80c
verb55nta <verb55nta@gmail.com>
2020-12-25 00:18:28 +0900

    add digURL and dump json

--- code-extractor/code_extractor/getCodes.py
+++ code-extractor/code_extractor/getCodes.py
@@ -1,7 +1,11 @@
+import json
 import re
+from datetime import datetime
 
 import requests
 from bs4 import BeautifulSoup
+
+# from datetime import
 
 
 def getDocumentBodyFromURL(url):
@@ -19,20 +23,23 @@ def getMessages(html_str):
     return obj.find_all('div', class_='message')
 
 
-def getCodes(filepath=None, url=None, sortOps=False):
-    if filepath is not None:
-        html_str = getDocumentBodyFromFile(filepath)
-    elif url is not None:
-        html_str = getDocumentBodyFromURL(url)
-    else:
-        ValueError('Please input filepath or url')
+def getCodes(html_str=None, filepath=None, url=None, sortOps=True):
 
-    codeDict = {}
-    codeDictCache = {}
-    dropList = ["RSI", "PSR", "NHK", "EV", "ETF", "REIT", "NISA"]
+    if html_str is None:
+        if filepath is not None:
+            html_str = getDocumentBodyFromFile(filepath)
+        elif url is not None:
+            html_str = getDocumentBodyFromURL(url)
+        else:
+            ValueError('Please input filepath or url')
 
     obj = BeautifulSoup(html_str, 'html.parser')
     obj = obj.find_all('div', class_='message')
+
+    codeDict = {}
+    codeDictCache = {}
+    dropList = ["RSI", "PSR", "NHK", "EV",
+                "ETF", "REIT", "NISA", "SBI", "W", "VIX", "HTTP", "WWW", "WW", "IPO"]
 
     for i, ob in enumerate(obj):
         rawText = ob.text
@@ -64,3 +71,55 @@ def getCodes(filepath=None, url=None, sortOps=False):
         return sorted(res)[::-1]
 
     return res
+
+
+def getThreadTime(html_str):
+    obj = BeautifulSoup(html_str, 'html.parser')
+
+    obj = obj.find_all('span', class_='date')
+
+    start_time = obj[0].text
+    end_time = obj[max(len(obj) - 3, 0)].text
+
+    pattern = r'\d+'
+
+    time_list = []
+    for time_text in (start_time, end_time):
+        res = re.findall(pattern, time_text)
+        year = int(res[0])
+        month = int(res[1])
+        day = int(res[2])
+        hour = int(res[3])
+        minute = int(res[4])
+        second = int(res[5])
+        time_list.append(datetime(year, month, day, hour=hour,
+                                  minute=minute, second=second))
+
+    return tuple(time_list)
+
+
+def digURL(url):
+    html_str = getDocumentBodyFromURL(url)
+    obj = BeautifulSoup(html_str, 'html.parser')
+    title = obj.find('title').text
+    obj = obj.find_all('div', class_='message')
+
+    start_time, end_time = getThreadTime(html_str)
+
+    codes = getCodes(html_str=html_str)
+
+    number = int(re.sub(r"\D", "", title))
+    dump_for_json = {}
+    dump_for_json["number"] = number
+
+    time_format = "%Y-%m-%dT%H:%M:%S"
+    dump_for_json["start_time"] = start_time.strftime(time_format)
+    dump_for_json["end_time"] = end_time.strftime(time_format)
+    dump_for_json["codes"] = {}
+    for c in codes:
+        dump_for_json["codes"][c[1]] = c[0]
+    with open(f'json/{number}.json', 'w') as f:
+        json.dump(dump_for_json, f, indent=2, ensure_ascii=False)
+
+    prev = obj[0].find_all('a')
+    return digURL(prev[-1].text)
--- code-extractor/code_extractor/main.py
+++ code-extractor/code_extractor/main.py
@@ -1,12 +1,9 @@
-from getCodes import getCodes
+from getCodes import digURL, getCodes
 
 
 def main():
-    filepath = '../tests/test_object/5ch_stock.html'
     url = 'https://egg.5ch.net/test/read.cgi/stock/1608794163/'
-    #codes = getCodes(filepath=filepath, sortOps=True)
-    codes = getCodes(url=url, sortOps=True)
-    print(codes[:10])
+    digURL(url)
 
 
 if __name__ == "__main__":
